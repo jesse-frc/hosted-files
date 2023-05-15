41eee85d42b7e79c9fc9c82c6118095234d26a30
jesse <jesse@flatratecopiers.com>
2023-05-15 17:26:35 -0400

    added zerofill function

--- cadrecrew/crm/js/str-editor.js
+++ cadrecrew/crm/js/str-editor.js
@@ -1,53 +1,3 @@
-function BootEditor(element_id) {
-    ClassicEditor
-    .create(document.querySelector(element_id), {
-        toolbar: {
-            items: [
-                'heading',
-                '|',
-                'bold',
-                'italic',
-                'link',
-                'bulletedList',
-                'numberedList',
-                '|',
-                'indent',
-                'outdent',
-                '|',
-                'imageUpload',
-                'blockQuote',
-                'insertTable',
-                'mediaEmbed',
-                'undo',
-                'redo'
-            ]
-        },
-        autoParagraph: true,
-        language: 'en',
-        image: {
-            toolbar: [
-                'imageTextAlternative',
-                'imageStyle:full',
-                'imageStyle:side'
-            ]
-        },
-        table: {
-            contentToolbar: [
-                'tableColumn',
-                'tableRow',
-                'mergeTableCells'
-            ]
-        },
-        licenseKey: '',
-    })
-    .then( editor => {
-        window['EmailContent'] = editor;
-    });
-}
-
-
-
-
 function CreateEditor(element_id) {
     ClassicEditor
         .create(document.querySelector(element_id), {
@@ -94,3 +44,11 @@ function CreateEditor(element_id) {
             window[element_id.substr(1)] = editor;
         });
 }
+
+
+function ZeroFill(number, width = 6) {
+    let numberOutput = Math.abs(number).toString();
+    let pad = '0'.repeat(Math.max(0, width - numberOutput.length));
+
+    return (number < 0 ? '-' : '') + pad + numberOutput;
+},
